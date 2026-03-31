89775cc30f42f64af82b91b26359f554d5de83ad
2564276 <2564276@cegeplimoilou.ca>
2026-03-31 13:04:21 -0400

    Implemente setteur getteur constructeur et resistance de la classe Composant

--- APP4/untitled/src/electronique/Composant.java
+++ APP4/untitled/src/electronique/Composant.java
@@ -1,6 +1,19 @@
 package electronique;
 
 public abstract class Composant {
-    public Composant() {
+    protected double resistance;
+    public Composant(double resistance) {
+        setResistance(resistance);
+    }
+    public double calculerResistance(){
+        return 0.0;
+    }
+
+    public double getResistance() {
+        return resistance;
+    }
+
+    public void setResistance(double resistance) {
+        this.resistance = resistance;
     }
 }
