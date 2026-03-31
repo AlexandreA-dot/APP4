ea1d05bf33a0c7d7d4c40e7c5ae879c4cf6ce401
2564276 <2564276@cegeplimoilou.ca>
2026-03-31 13:31:09 -0400

    Implimenter methode CalculerResistence pour les classe CircuitParallele et CircuitSerie

--- APP4/untitled/src/electronique/CircuitParallele.java
+++ APP4/untitled/src/electronique/CircuitParallele.java
@@ -2,5 +2,15 @@ package electronique;
 
 public class CircuitParallele extends Circuit{
     public CircuitParallele() {
+        super();
     }
+    //// Liste Composant pas encore implimenter
+//    @Override
+//    public double calculerResistance() {
+//        double reponse = 0.0;
+//        for (Composant composant: listeComposant){
+//            reponse += (1 / composant.calculerResistance());
+//        }
+//        return reponse;
+//    }
 }
--- APP4/untitled/src/electronique/CircuitSerie.java
+++ APP4/untitled/src/electronique/CircuitSerie.java
@@ -2,5 +2,15 @@ package electronique;
 
 public class CircuitSerie extends Circuit{
     public CircuitSerie() {
+        super();
     }
+//// Liste Composant pas encore implimenter
+//    @Override
+//    public double calculerResistance() {
+//        double reponse = 0.0;
+//        for (Composant composant: listeComposant){
+//            reponse += composant.calculerResistance();
+//        }
+//        return reponse;
+//    }
 }
