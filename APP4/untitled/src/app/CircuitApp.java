51bf1f4ae202c2461d5f98f493ff0be0aef0cc45
2564276 <2564276@cegeplimoilou.ca>
2026-04-07 13:27:52 -0400

    Implimenter methode CircuitBuilder

--- APP4/untitled/src/app/CircuitApp.java
+++ APP4/untitled/src/app/CircuitApp.java
@@ -6,5 +6,6 @@ public class CircuitApp {
 
     public static void main(String[] args) {
 
+
     }
 }
--- APP4/untitled/src/app/CircuitBuilder.java
+++ APP4/untitled/src/app/CircuitBuilder.java
@@ -1,17 +1,38 @@
 package app;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import electronique.CircuitParallele;
 import electronique.CircuitSerie;
 import electronique.Composant;
 import electronique.Resistance;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import java.util.List;
 
 
 public class CircuitBuilder {
+    private static final char fSep = File.separatorChar;
+    private static String pathIn = System.getProperty("user.dir") + fSep + "APP4" + fSep + "src" + fSep + "donnees" + fSep;
+
+    public Composant ConstruireCircuit(String Fichier) {
+        pathIn += Fichier;
+        List<Composant> listeComposant = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode donneesCircuit = mapper.readTree(new File(pathIn));
+            return lireCircuit(donneesCircuit);
+        } catch (IOException e) {
+
+            System.err.println("Erreur de lecture : " + e.getMessage());
+
+        }
+        return null;
+    }
+
     /**
      * Méthode récursive permettant de lire et de créer un circuit à partir d'un nœud JSON.
      *
