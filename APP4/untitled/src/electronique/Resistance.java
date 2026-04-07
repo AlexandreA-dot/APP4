d01d6c5a8409eb201affe79d7deb8ab1f53b3d2c
2564276 <2564276@cegeplimoilou.ca>
2026-04-07 11:46:55 -0400

    Implimenter Resistance par defaut si pas mentionner

--- APP4/untitled/src/electronique/Resistance.java
+++ APP4/untitled/src/electronique/Resistance.java
@@ -2,8 +2,14 @@ package electronique;
 
 public class Resistance {
     private double resistance;
+    private final double RESISTANCE_DEFAUT = 5.0;
+
     public Resistance(double pResistance) {
         setResistance(pResistance);
+    }
+
+    public Resistance() {
+        setResistance(RESISTANCE_DEFAUT);
     }
 
     public double calculerResistance() {
